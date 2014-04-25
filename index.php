ad209a9325c813ad0c988e0d975e8949b3450d10
Stoica Alex <alex_stoicajr@yahoo.co.uk>
2014-04-25 13:36:17 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,59 @@
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Stoica Alex's Blog</title>
+	<link rel="stylesheet" href="css/fc-webicons.css">
+	<link rel="stylesheet" href="css/style.css">
+	<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-27142889-3', 'alexstoica.com');
+  ga('send', 'pageview');
+
+</script>
+</head>
+
+
+<body>
+	<div class="container">
+
+      <div class="jumbotron">
+				<!-- <h1>Hello!</h1>  -->
+				<img class="me" src="img/me.jpg"></img>
+				<p class="lead">I work on amazing stuff, below are a couple of my latest projects. <br><br>You can contact me here:</p>
+				<p>
+					<a class="fc-webicon github large" onClick="_gaq.push(['_trackEvent', 'Github' , 'Clicked Github']);" href="https://github.com/alexstoick">Github</a>
+					<a class="fc-webicon mail large" onClick="_gaq.push(['_trackEvent', 'Email', 'Clicked Email']);" href="mailto:alex.stoicajr@gmail.com">Mail</a>
+					<a class="fc-webicon twitter large" onClick="_gaq.push(['_trackEvent', 'Twitter', 'Clicked Twitter']);" href="https://twitter.com/alexstoick">Twitter</a>
+					<a class="fc-webicon linkedin large" onClick="_gaq.push(['_trackEvent', 'LinkedIn', 'Clicked LinkedIn']);" href="http://uk.linkedin.com/pub/alexandru-stoica/85/209/70b/">LinkedIn</a>
+				</p>
+      </div>
+
+      <div class="row">
+        <div class="col-lg-4">
+          <img class="me" src="img/QuestionMark.jpg" alt="Bought.Today">
+          <h2><a href="http://bought.today" alt="Bought.Today" class="link-no-style">Bought.Today</a></h2>
+          <p> A project that will revolutionize the way you think about receipts - by providing people with itemized electronic
+          	receipts. </p>
+        </div><!-- /.col-lg-4 -->
+        <div class="col-lg-4">
+          <img class="me" src="img/PropertyWatch.jpg" alt="PropertyWatch">
+          <h2><a class="link-no-style" href="http://alexstoica.com/PropertyWatch/">PropertyWatch</a></h2>
+          <p> A small iOS app I developed for myself to check the prices of properties in certain zones, and
+          get notified when new property that match my criteria become available.</p>
+        </div><!-- /.col-lg-4 -->
+        <div class="col-lg-4">
+          <img class="me" src="img/Akin1024.png" alt="Akin.is">
+          <h2><a class="link-no-style" href="http://akin.is">Akin.is</a></h2>
+          <p> The startup I am currently interning for is developing this cool tool to help you meet new and interesting people.</p>
+        </div><!-- /.col-lg-4 -->
+      </div><!-- /.row -->
+	</div> <!-- /container -->
+
+    <script src='js/modernizr.min.js'></script>
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+</body>
+</html>
