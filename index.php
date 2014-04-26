c359305fedef123f3121906723336927f06feda7
Stoica Alex <alex_stoicajr@yahoo.co.uk>
2014-04-26 11:30:46 +0300

    fiddling with the fonts & background color

--- index.php
+++ index.php
@@ -1,19 +1,35 @@
 <html lang="en">
 <head>
+
+  <meta content='Alexandru Stoica' name='author'>
+  <meta content='width=device-width, initial-scale=1' name='viewport'>
+  <meta content='Personal website of Alexandru Stoica. Holds latest projects.' name='description'>
+  <meta content="Alexandru Stoica, Running, King's College London, PropertyWatch  " name='keywords'>
+  <meta content='text/html;charset=UTF-8' http-equiv='content-type'>
+  <meta content='ALL' name='robots'>
+  <meta content='1 Day' name='revisit-after'>
+  <meta content='Never' name='expires'>
+  <meta content='TRUE' name='MSSmartTagsPreventParsing'>
+  <meta content='http://alexstoica.com' name='Identifier-URL'>
+  <meta content='http://alexstoica.com' name='Home_URL'>
+  <meta content='en-gb' http-equiv='Content-Language'>
+
 	<meta charset="utf-8">
-	<title>Stoica Alex's Blog</title>
+	<title>Stoica Alex's Website</title>
+  <link href='http://fonts.googleapis.com/css?family=Lato:300' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Roboto:300,100,100italic' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="css/fc-webicons.css">
 	<link rel="stylesheet" href="css/style.css">
 	<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-27142889-3', 'alexstoica.com');
-  ga('send', 'pageview');
+    ga('create', 'UA-27142889-3', 'alexstoica.com');
+    ga('send', 'pageview');
 
-</script>
+  </script>
 </head>
 
 
@@ -21,9 +37,9 @@
 	<div class="container">
 
       <div class="jumbotron">
-				<!-- <h1>Hello!</h1>  -->
-				<img class="me" src="img/me.jpg"></img>
-				<p class="lead">I work on amazing stuff, below are a couple of my latest projects. <br><br>You can contact me here:</p>
+				<img class="me" src="img/me.png"></img>
+        <p class="lead">I am a first year student at <span class="light-italic">King's College London</span>. I'm super passionate about Computer Science and an avid runner. </p>
+				<p class="lead">I work on amazing stuff, below are a couple of my latest projects. <br><br>Contact me here:</p>
 				<p>
 					<a class="fc-webicon github large" onClick="_gaq.push(['_trackEvent', 'Github' , 'Clicked Github']);" href="https://github.com/alexstoick">Github</a>
 					<a class="fc-webicon mail large" onClick="_gaq.push(['_trackEvent', 'Email', 'Clicked Email']);" href="mailto:alex.stoicajr@gmail.com">Mail</a>
@@ -34,16 +50,16 @@
 
       <div class="row">
         <div class="col-lg-4">
-          <img class="me" src="img/QuestionMark.jpg" alt="Bought.Today">
+          <img class="me" src="img/QuestionMark.png" alt="Bought.Today">
           <h2><a href="http://bought.today" alt="Bought.Today" class="link-no-style">Bought.Today</a></h2>
           <p> A project that will revolutionize the way you think about receipts - by providing people with itemized electronic
           	receipts. </p>
         </div><!-- /.col-lg-4 -->
         <div class="col-lg-4">
-          <img class="me" src="img/PropertyWatch.jpg" alt="PropertyWatch">
+          <img class="me" src="img/PropertyWatch.png" alt="PropertyWatch">
           <h2><a class="link-no-style" href="http://alexstoica.com/PropertyWatch/">PropertyWatch</a></h2>
           <p> A small iOS app I developed for myself to check the prices of properties in certain zones, and
-          get notified when new property that match my criteria become available.</p>
+          get notified when new properties that match my criteria become available.</p>
         </div><!-- /.col-lg-4 -->
         <div class="col-lg-4">
           <img class="me" src="img/Akin1024.png" alt="Akin.is">
@@ -52,7 +68,6 @@
         </div><!-- /.col-lg-4 -->
       </div><!-- /.row -->
 	</div> <!-- /container -->
-
     <script src='js/modernizr.min.js'></script>
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 </body>
