37409caa5c2858d8320bf7f5d44a3636f7f56da5
Stoica Alex <alex_stoicajr@yahoo.co.uk>
2016-01-08 14:52:33 +0200

    changed some information

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   <meta content='Alexandru Stoica' name='author'>
   <meta content='width=device-width, initial-scale=1' name='viewport'>
   <meta content='Personal website of Alexandru Stoica. Holds latest projects.' name='description'>
-  <meta content="Alexandru Stoica, Running, King's College London, PropertyWatch" name='keywords'>
+  <meta content="Alexandru Stoica, Running, Developer, Ruby, Backend" name='keywords'>
   <meta content='text/html;charset=UTF-8' http-equiv='content-type'>
   <meta content='ALL' name='robots'>
   <meta content='1 Day' name='revisit-after'>
@@ -15,7 +15,7 @@
   <meta content='en-gb' http-equiv='Content-Language'>
 
 	<meta charset="utf-8">
-	<title>Stoica Alex's Website</title>
+	<title>Stoica Alexandru's Website</title>
   <link href='http://fonts.googleapis.com/css?family=Lato:300' rel='stylesheet' type='text/css'>
   <link href='http://fonts.googleapis.com/css?family=Roboto:300,100,100italic' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="css/fc-webicons.css">
@@ -37,33 +37,14 @@
 
       <div class="jumbotron">
   				<img class="me" src="img/me.png"></img>
-          <p class="lead">I am a second year student at <span class="special">King's College London</span>. I'm super passionate about <span class="special">Computer Science</span> and an avid <span class="special">runner</span>. </p>
-  				<p class="lead">I work on amazing stuff, below are a couple of my latest projects. <br></p>
+          <p class="lead">I'm super passionate about <span class="special">Computer Science</span> and an avid <span class="special">runner</span>. </p>
+  				<p class="lead">I'm currently a backend developer at <a class="special" style="text-decoration: none" href="http://lostmy.name">Lostmy.name</a></p>
+          <!--  I work on amazing stuff, below are a couple of my latest projects. <br></p> -->
   				<a class="fc-webicon github large" onClick="_gaq.push(['_trackEvent', 'click' , 'Clicked Github']);" href="https://github.com/alexstoick">Github</a>
   				<a class="fc-webicon mail large" onClick="_gaq.push(['_trackEvent', 'click', 'Clicked Email']);" href="mailto:alex.stoicajr@gmail.com">Mail</a>
   				<a class="fc-webicon twitter large" onClick="_gaq.push(['_trackEvent', 'click', 'Clicked Twitter']);" href="https://twitter.com/alexstoick">Twitter</a>
   				<a class="fc-webicon linkedin large" onClick="_gaq.push(['_trackEvent', 'click', 'Clicked LinkedIn']);" href="http://uk.linkedin.com/pub/alexandru-stoica/85/209/70b/">LinkedIn</a>
       </div>
-
-      <div class="row">
-        <div class="col-lg-4">
-          <img class="me" src="img/QuestionMark.png" alt="Bought.Today">
-          <h2><a href="http://bought.today" alt="Bought.Today" class="link-no-style">Bought.Today</a></h2>
-          <p> A project that will revolutionize the way you think about receipts - by providing people with itemized electronic
-          	receipts. </p>
-        </div><!-- /.col-lg-4 -->
-        <div class="col-lg-4">
-          <img class="me" src="img/PropertyWatch.png" alt="PropertyWatch">
-          <h2><a class="link-no-style" href="http://alexstoica.com/PropertyWatch/">PropertyWatch</a></h2>
-          <p> A small iOS app I developed to check the prices of properties in certain zones, and
-          get notified when new properties that match my criteria become available.</p>
-        </div><!-- /.col-lg-4 -->
-        <div class="col-lg-4">
-          <img class="me" src="img/Akin1024.png" alt="Akin.is">
-          <h2><a class="link-no-style" href="http://akin.is">Akin.is</a></h2>
-          <p> The startup I am currently interning for is developing this cool tool to help you meet new and interesting people.</p>
-        </div><!-- /.col-lg-4 -->
-      </div><!-- /.row -->
 	</div> <!-- /container -->
     <script src='js/modernizr.min.js'></script>
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
